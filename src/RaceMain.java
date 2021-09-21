452239e1a14dd1b063280c34696e684f82797401
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-09-21 16:04:18 -0400

    Add files via upload

---
+++ src/RaceMain.java
@@ -0,0 +1,18 @@
+public class RaceMain
+{
+    public static void main(String[] args)
+    {
+        // Length of the course in meters
+        double distance = 2414; // ~ 1.5 miles
+        
+        // Generate a random acceleration for each car
+        
+        
+        // Create two Racecar objects
+        
+        // Compute the finishing times for both cars
+        
+        // Print times of each car
+        
+    }
+}
---
+++ src/Racecar.java
@@ -0,0 +1,23 @@
+public class Racecar
+{
+    private double accel; // acceleration
+    private String name; // name of driver
+    
+    public Racecar(double acceleration, String driver)
+    {
+        accel = acceleration;
+        name = driver;
+    }
+    
+    // Returns the time it takes the racecar
+    // to complete the track
+    public double computeTime(double distance)
+    {
+
+    }
+    
+    public String toString()
+    {
+        return "Racer " + name;
+    }
+}
